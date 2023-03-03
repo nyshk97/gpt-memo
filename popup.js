e99c0ec392966ade17335b7f9c1a5a18f481f8f6
tsubasa namatame <nyshk97@gmail.com>
2023-03-03 16:58:01 +0900

    ボタンBを追加

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var submitBtn = document.getElementById('submit-btn');
+  var buttonA = document.getElementById('button-a');
+  var buttonB = document.getElementById('button-b');
   var inputText = document.getElementById('input-text');
 
-  submitBtn.addEventListener('click', function () {
-    var text = inputText.value + 'hogehoge';
-    var copyToClipboard = function (text) {
-      var copyElement = document.createElement('textarea');
-      copyElement.style.opacity = '0';
-      copyElement.style.position = 'absolute';
-      copyElement.textContent = text;
-      document.body.appendChild(copyElement);
-      copyElement.select();
-      document.execCommand('copy');
-      document.body.removeChild(copyElement);
-    };
+  buttonA.addEventListener('click', function () {
+    var text = inputText.value + 'AAA';
     copyToClipboard(text);
   });
+
+  buttonB.addEventListener('click', function () {
+    var text = inputText.value + 'BBB';
+    copyToClipboard(text);
+  });
+
+  function copyToClipboard(text) {
+    var copyElement = document.createElement('textarea');
+    copyElement.style.opacity = '0';
+    copyElement.style.position = 'absolute';
+    copyElement.textContent = text;
+    document.body.appendChild(copyElement);
+    copyElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyElement);
+  }
 });
